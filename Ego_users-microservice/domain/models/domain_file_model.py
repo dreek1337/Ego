c52c152621910f493c58ed2a51264ba076cb8933
danila <danil.sapog19@gmail.com>
2023-05-07 12:26:16 +0300

    replace files

--- Ego_users-microservice/domain/models/domain_file_model.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field, UUID4
-
-
-class File(BaseModel):
-    id: int = Field(..., 'Идентификатор файла')
-    file_uuid: UUID4 = Field(..., 'Имя файла в S3')
-    size: int = Field(..., 'Размер файла')
-    type: str = Field(..., 'Тип файла')
