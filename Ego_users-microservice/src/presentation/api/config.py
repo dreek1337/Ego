4839fc344fcd07e54e1da1c64e2321fbaa51bb65
danila <danil.sapog19@gmail.com>
2023-06-15 20:50:58 +0300

    refact config

--- Ego_users-microservice/src/presentation/api/config.py
+++ Ego_users-microservice/src/presentation/api/config.py
@@ -11,7 +11,6 @@ class APIConfig(BaseSettings):
     host: str = Field(..., env="SITE_HOST")
     port: int = Field(..., env="SITE_PORT")
     loop: str = Field("asyncio")
-    reload_delay: float = Field(0.25, env="SITE_RELOAD_DELAY")
 
     class Config:
         env_file = ".env"
