69ce1a272578ca6adf6fba2f5b6019eaad3e31a2
danila <danil.sapog19@gmail.com>
2023-07-11 02:19:30 +0300

    config for path env in BaseSettings

--- auth_microservice/src_auth/config/settings/base.py
+++ auth_microservice/src_auth/config/settings/base.py
@@ -3,5 +3,6 @@ from pydantic import BaseSettings
 
 class BaseConfigSettings(BaseSettings):
     class Config:
+        env_prefix = "auth_microservice"
         env_file = ".env"
         env_file_encoding = "utf-8"
