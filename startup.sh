7cd719533b44e3ccd31dde1b1685ac6332a0ae9d
danila <danil.sapog19@gmail.com>
2023-07-13 20:47:21 +0300

    refact startup settings

--- startup.sh
+++ startup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+cd /posts_microservice
 
-sudo docker compose -f posts_microservice/docker-compose.yaml up setup
-
-sudo docker compose -f posts_microservice/docker-compose.yaml up -d
-
-sudo docker compose -f auth_microservice/docker-compose.yaml up -d
-
-sudo docker compose -f users_microservice/docker-compose.yaml up -d
+sudo docker compose up setup
 
 sudo docker compose up -d
 
-sudo docker exec apisix-con bash -c "/usr/local/apisix/configure_variables.sh"
+cd ../auth_microservice
+
+sudo docker compose up -d
+
+cd ../users_microservice
+
+sudo docker compose up -d
+
+
+cd ../
+
+sudo docker compose up -d
+
+sudo docker exec apisix-con bash -c "configure_variables.sh"
