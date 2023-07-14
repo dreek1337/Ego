bed460ee8e09a5220c5b63dd43f699a79c143365
danila <danil.sapog19@gmail.com>
2023-07-14 12:12:21 +0300

    configure elasticsearch settings

--- startup.sh
+++ startup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# Clean Docker
+sudo docker stop $(sudo docker ps -qa)
+sudo docker rm $(sudo docker ps -qa)
+sudo docker rmi -f $(sudo docker images -qa)
+sudo docker volume rm $(sudo docker volume ls -q)
+sudo docker network rm $(sudo docker network ls -q)
+
+# Configure Docker
 sudo docker compose -f posts_microservice/docker-compose.yaml up setup
-
 sudo docker compose -f posts_microservice/docker-compose.yaml up -d
-
 sudo docker compose -f auth_microservice/docker-compose.yaml up -d
-
 sudo docker compose -f users_microservice/docker-compose.yaml up -d
-
 sudo docker compose up -d
 
+# Start bash script into container
 sudo docker exec apisix-con bash -c "./configure_variables.sh"
