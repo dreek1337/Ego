cc631335a3bde44ceb64756b04fef1172785152e
danila <danil.sapog19@gmail.com>
2023-07-18 13:58:48 +0300

    change startup.sh

--- startup.sh
+++ startup.sh
@@ -2,9 +2,6 @@
 # Add variables
 source .env
 
-# Reset iptables to the default settings
-sudo iptables -F
-
 # Clean Docker
 sudo docker stop $(sudo docker ps -qa)
 sudo docker rm $(sudo docker ps -qa)
